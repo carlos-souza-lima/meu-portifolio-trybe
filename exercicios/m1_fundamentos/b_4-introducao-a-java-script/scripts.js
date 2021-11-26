31ac236f691a4f1e3bc48f33a07619dbfa97fc7f
José <carlossouzalimajose@gmail.com>
2021-11-26 15:57:42 -0300

    variaveis

---
+++ exercicios/m1_fundamentos/b_4-introducao-a-java-script/scripts.js
@@ -0,0 +1,11 @@
+const myName = "José";
+const birthCity = "São Leopoldo";
+let birthYear = 18;
+
+console.log(myName, birthYear, birthCity);
+
+birthYear = 2030;
+console.log(birthYear);
+
+birthCity = "porto Alegre";
+console.log(birthCity);
