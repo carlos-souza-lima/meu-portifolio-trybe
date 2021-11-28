56ef4f09fa48e92332abeefe661eb510c5b3b24c
José <carlossouzalimajose@gmail.com>
2021-11-28 12:31:21 -0300

    video para praticar ate o 3

--- exercicios/m1_fundamentos/b_4-introducao-a-java-script/scripts.js
+++ exercicios/m1_fundamentos/b_4-introducao-a-java-script/scripts.js
@@ -1,4 +1,5 @@
-const myName = "José";
+//video 1 
+/* const myName = "José";
 const birthCity = "São Leopoldo";
 let birthYear = 18;
 
@@ -9,3 +10,54 @@ console.log(birthYear);
 
 birthCity = "porto Alegre";
 console.log(birthCity);
+ */
+
+
+
+//video 2
+
+
+/* let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+
+console.log(typeof patientId)
+
+console.log(typeof isEnrolled)
+
+console.log(typeof patientInfo)
+
+console.log(typeof patientEmail) */
+
+
+/* const base = 5;
+const height = 8;
+
+const area = base * height;
+console.log(area); */
+
+/* const perimeter = (base * 2) + (height * 2); 
+console.log(perimeter);
+ */
+/* const perimeter = (((base * 2) + (height * 2)) ** 2); 
+console.log(perimeter); */
+
+
+//VIDEO 3 
+
+const nota = 74.5;
+
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovada(o)"); 
+}
+else if (nota < 80 && nota >= 60) {
+    console.log("Você está na nossa lista de espera");
+}
+else {
+    console.log("Você foi reprovada(o)");
+}
