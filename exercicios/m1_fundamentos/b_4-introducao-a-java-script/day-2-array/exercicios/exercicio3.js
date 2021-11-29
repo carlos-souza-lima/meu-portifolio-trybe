45bf676f5295877704d1519ec16fd662c9644422
José <carlossouzalimajose@gmail.com>
2021-11-29 18:22:38 -0300

    exercicio 3 para fixar

--- exercicios/m1_fundamentos/b_4-introducao-a-java-script/day-2-array/exercicios/exercicio3.js
+++ exercicios/m1_fundamentos/b_4-introducao-a-java-script/day-2-array/exercicios/exercicio3.js
@@ -1,3 +1,12 @@
 /* EXERCICIOS PARA FIXAR */
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27, 56, 57, 2];
+
+/* EXERCICIO 3 */
+let soma = 0;
+console.log(numbers.length);
+for (let index2 = 0; index2 < numbers.length; index2 += 1) {
+    soma = soma + (numbers[index2]);
+}
+   let mediaAritimetica = soma / (numbers.length)
+console.log('A média aritimetica é igual: ', mediaAritimetica);
