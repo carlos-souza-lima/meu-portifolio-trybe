41c6f458efb58006a37dfa1ada7a05e2a8d994e2
José <carlossouzalimajose@gmail.com>
2021-11-28 22:24:22 -0300

    programas parte dois rodando

--- exercicios/m1_fundamentos/b_4-introducao-a-java-script/day-1-primeiros-passos-javascript/exercicios/scripts.js
+++ exercicios/m1_fundamentos/b_4-introducao-a-java-script/day-1-primeiros-passos-javascript/exercicios/scripts.js
@@ -1,7 +1,9 @@
 /* SEÇÃO DAS VARIAVEIS E CONSTANTES */
 const firstNumber = 20;
-const secondNumber = 10;
+const secondNumber = 100;
 let resultOperacao ;
+
+/* PARTE 1 */
 
 /* 1° PROGRAMA  ADIÇÃO */
 resultOperacao = firstNumber + secondNumber;
@@ -28,3 +30,11 @@ resultOperacao = firstNumber % secondNumber;
 console.log(resultOperacao);
 resultOperacao = secondNumber % firstNumber;
 console.log(resultOperacao);
+
+/* PARTE 2 */
+/* retorne o maior numero */
+if (firstNumber > secondNumber) {
+    console.log("O maior numero é: ", firstNumber);
+} else {
+    console.log("o maior numero é: ", secondNumber);
+}
