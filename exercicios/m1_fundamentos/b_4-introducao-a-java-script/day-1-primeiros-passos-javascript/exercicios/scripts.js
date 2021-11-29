d51b20dc6175ca2698b0c79dd48a1c6df1ea8d6d
José <carlossouzalimajose@gmail.com>
2021-11-28 21:58:08 -0300

    ajeitando a estrutura do codigo

--- exercicios/m1_fundamentos/b_4-introducao-a-java-script/day-1-primeiros-passos-javascript/exercicios/scripts.js
+++ exercicios/m1_fundamentos/b_4-introducao-a-java-script/day-1-primeiros-passos-javascript/exercicios/scripts.js
@@ -1,10 +1,11 @@
-/* 1° PROGRAMA */
-
 /* SEÇÃO DAS VARIAVEIS E CONSTANTES */
 const firstNumber = 10;
 const secondNumber = 35;
 let resultOperacao ;
 
+/* 1° PROGRAMA  ADIÇÃO */
 resultOperacao = firstNumber + secondNumber;
 console.log(resultOperacao);
+
+/* 2° PROGRAMA  */
 
