aa3266f540e49103fbac5bee333bdaf57ef36b98
José <carlossouzalimajose@gmail.com>
2021-11-29 17:59:35 -0300

    exercicio 1, para praticar

--- exercicios/m1_fundamentos/b_4-introducao-a-java-script/day-2-array/exercicios/script.js
+++ exercicios/m1_fundamentos/b_4-introducao-a-java-script/day-2-array/exercicios/script.js
@@ -1,2 +1,9 @@
 /* EXERCICIOS PARA FIXAR */
 
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+/* EXERCICIO 1 */
+
+for (index = 0; index < numbers.length; index+=1) {
+    console.log(numbers[index]);
+}
