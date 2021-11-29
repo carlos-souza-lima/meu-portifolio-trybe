ab26c8b5bd33c49b477007c4da387feecd5742ce
José <carlossouzalimajose@gmail.com>
2021-11-29 18:12:57 -0300

    organizando a estrutura

--- exercicios/m1_fundamentos/b_4-introducao-a-java-script/day-2-array/exercicios/exercicio1.js
+++ exercicios/m1_fundamentos/b_4-introducao-a-java-script/day-2-array/exercicios/exercicio1.js
@@ -6,10 +6,3 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let index = 0; index < numbers.length; index += 1) {
     console.log(numbers[index]);
 }
-
-/* EXERCICIO 2  */
-let soma = 0;
-for (let index2 = 0; index2 < numbers.length; index2 += 1) {
-    soma = soma + (numbers[index2]);
-}
-console.log('Total da soma dos valores:', soma);
