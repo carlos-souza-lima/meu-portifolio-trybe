3608331a4a541bf0f35475d9b751cf81a93e6edb
José <carlossouzalimajose@gmail.com>
2021-11-29 16:02:16 -0300

    exercicio 2

--- exercicios/m1_fundamentos/b_4-introducao-a-java-script/day-2-array/videos/script2.js
+++ exercicios/m1_fundamentos/b_4-introducao-a-java-script/day-2-array/videos/script2.js
@@ -1,5 +1,15 @@
+
+/* EXERCICIO 1 */
 let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 
 for (contador = 0; contador < groceryList.length; contador += 1) {
     console.log(groceryList[contador]);
+}
+
+/* EXERCICIO 2 */
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let sum of names)
+{
+    console.log(sum);
 }
