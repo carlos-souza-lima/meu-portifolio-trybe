c08865850da2575e30d2e8de074aef472d291dc5
José <carlossouzalimajose@gmail.com>
2021-11-28 22:03:28 -0300

    terceiro programa (multiplicaça ) rodando

--- exercicios/m1_fundamentos/b_4-introducao-a-java-script/day-1-primeiros-passos-javascript/exercicios/scripts.js
+++ exercicios/m1_fundamentos/b_4-introducao-a-java-script/day-1-primeiros-passos-javascript/exercicios/scripts.js
@@ -7,6 +7,12 @@ let resultOperacao ;
 resultOperacao = firstNumber + secondNumber;
 console.log(resultOperacao);
 
-/* 2° PROGRAMA  */
+/* 2° PROGRAMA SUBTRAÇÃO  */
 resultOperacao = firstNumber - secondNumber;
 console.log(resultOperacao); 
+resultOperacao = secondNumber - firstNumber;
+console.log(resultOperacao);
+
+/* 3° PROGRAMA MULTIPLICAÇÃO */
+resultOperacao = firstNumber * secondNumber; 
+console.log( resultOperacao);
