89349f95cb02af76c2a54dea0c7ef29d7c66f0da
José <carlossouzalimajose@gmail.com>
2021-11-28 21:56:08 -0300

    primeiro programa (adiçao) rodando

---
+++ exercicios/m1_fundamentos/b_4-introducao-a-java-script/day-1-primeiros-passos-javascript/exercicios/scripts.js
@@ -0,0 +1,10 @@
+/* 1° PROGRAMA */
+
+/* SEÇÃO DAS VARIAVEIS E CONSTANTES */
+const firstNumber = 10;
+const secondNumber = 35;
+let resultOperacao ;
+
+resultOperacao = firstNumber + secondNumber;
+console.log(resultOperacao);
+
