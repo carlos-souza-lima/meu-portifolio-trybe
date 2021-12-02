413ad0e153bf124e53507e45a99c62a9e13d5546
José <carlossouzalimajose@gmail.com>
2021-12-02 09:13:29 -0300

    exercicio 6 concluido

--- exercicios/m1_fundamentos/b_4-introducao-a-java-script/day-4-objetos/exercicios/parte1.js
+++ exercicios/m1_fundamentos/b_4-introducao-a-java-script/day-4-objetos/exercicios/parte1.js
@@ -53,4 +53,4 @@ let leitor = {
     ],
   };
  livro = leitor['livrosFavoritos']
-  console.log("O livro favorito de", leitor.nome, leitor.sobrenome, "se chama", leitor.livrosFavoritos)
+  console.log("O livro favorito de", leitor.nome, leitor.sobrenome, "se chama", '"'+ leitor.livrosFavoritos[0]['titulo']+'"')
