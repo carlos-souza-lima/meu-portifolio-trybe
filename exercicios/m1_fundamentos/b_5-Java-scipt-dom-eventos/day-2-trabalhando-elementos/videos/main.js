3a224e0c8d8936b1ee98e63d68cc6c5b53647c4e
José <carlossouzalimajose@gmail.com>
2021-12-06 19:17:35 -0300

    exercicio 1, criando o h1

---
+++ exercicios/m1_fundamentos/b_5-Java-scipt-dom-eventos/day-2-trabalhando-elementos/videos/main.js
@@ -0,0 +1,10 @@
+let paiElementos = document.querySelector('#pai');
+let text = 'teste de elementos'
+
+let novoFilhoDoPai = document.createElement('section');
+
+novoElementoDoFilhoDoPai = document.createElement('p');
+novoElementoDoFilhoDoPai.innerText = text
+novoFilhoDoPai.innerHTML =  novoElementoDoFilhoDoPai.
+
+paiElementos.appendChild(novoFilhoDoPai)
