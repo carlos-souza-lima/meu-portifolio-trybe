1960ececbcb638dfa0e0ebfd0a236618f702eafa
José <carlossouzalimajose@gmail.com>
2021-12-08 00:05:51 -0300

    1 tentativa de resolver o exercicio 3

--- exercicios/m1_fundamentos/b_5-Java-scipt-dom-eventos/day-3-eventos/exercicios/script.js
+++ exercicios/m1_fundamentos/b_5-Java-scipt-dom-eventos/day-3-eventos/exercicios/script.js
@@ -14,7 +14,7 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // EXERCICIO 1
-const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 function addNumbersDeze() {
   let ulDay = document.querySelector('#days');
   // console.log(ulDay);
@@ -47,3 +47,21 @@ function criaFeriados() {
   buttCont.appendChild(buttonFeriado);
 }
 criaFeriados()
+
+// EXERCICIO 3
+let buttFeriado = document.querySelector('#btn-holiday');
+console.log(buttFeriado)
+buttFeriado.addEventListener("click", 
+function ()  {
+  console.log('oi')
+  
+  // let diasFeriado = document.getElementsByClassName('day holiday');
+  // for (let i in diasFeriado){
+  //   diasFeriado[i]= document.getElementsByClassName('day holiday').style.color= 'red'; 
+  // }
+  })
+ 
+
+
+
+
