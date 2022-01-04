dd65e4ab2a1d88a2056203b7c513f64cee25323b
José <carlossouzalimajose@gmail.com>
2022-01-04 08:51:26 -0300

    finalizando o o dia 5.4

---
+++ exercicios/m1_fundamentos/b_5-Java-scipt-dom-eventos/day-4-web-storage/exercicios/script.js
@@ -0,0 +1,13 @@
+const cBcBody = document.querySelector('.cBcBody');
+for (let i = 0; i < 3; i += 1) {
+    const button = document.createElement('button');
+    button.classList.add('buttons');
+    cBcBody.appendChild(button);
+}
+
+const cBcSectionUm = document.querySelector('.cBcSectionUm');
+for (let i = 0; i < 3; i += 1) {
+    const button = document.createElement('button');
+    button.classList.add('buttons');
+    cBcSectionUm.appendChild(button);
+}
