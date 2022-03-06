8ee2ef992b8ad27000e6cd8f553d6803e928ce81
José Carlos <carlossouzalimajose@gmail.com>
2022-03-06 12:54:21 -0300

    exercicios bloco 10

--- exercicios/Front-End/Bloco-10/bloco-10.1/exercicio-2/exercicio2-react/src/App.js
+++ exercicios/Front-End/Bloco-10/bloco-10.1/exercicio-2/exercicio2-react/src/App.js
@@ -2,7 +2,7 @@ import './App.css'
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
-import React from 'react';
+// import React from 'react';
 
 function App () {
   // render() {
